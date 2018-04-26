d2b37d07a8c04fb49321e100b011666e05118e88
Ramadan Saputra <rarasta27@gmail.com>
2018-04-26 22:22:04 +0700

    Class Controller
    => update penyempurnaan method, dibuat final agar semua class yg mengextend gk bisa override method di parent nya
    => update method layout untuk layouting data dan template admin
    => update method view untuk pemanggilan view langsung tanpa layouting

--- app/library/Controller.class.php
+++ app/library/Controller.class.php
@@ -4,39 +4,44 @@
 	* Method Method Utama
 	*/
 	class Controller{
-		
-		public function __construct(){
-			
-		}
+		// public function __construct(){
 
-		protected function page(){
+		// }
+
+		final protected function page(){
 			$view = new Page();
 			return $view;
 		}
 
-		protected function model($modelName){
+		final protected function model($modelName){
 			require_once ROOT.DS.'app'.DS.'models'.DS.ucfirst($modelName).'.php';
 			$class = ucfirst($modelName);
 			$this->$modelName = new $class();
 		}
 
-		protected function layout($content, $config, $data = null){
+		final protected function auth($auth = 'auth'){
+			$this->$auth = new Auth();
+		}
+
+		final protected function layout($content, $config = null, $data = null){
 			$view = $this->page();
 
 			// set data
-			$view->setData($data);
+			if($data != null) $view->setData($data);
 
-			// set title
-			$view->setTitle($config['title']['main'], $config['title']['sub']);
+			if($config != null){
+				// set title
+				$view->setTitle($config['title']['main'], $config['title']['sub']);
 
-			// set css
-			foreach($config['css'] as $value){
-				$view->addCSS($value);
-			}
+				// set css
+				foreach($config['css'] as $value){
+					$view->addCSS($value);
+				}
 
-			// set js
-			foreach($config['js'] as $value){
-				$view->addJS($value);
+				// set js
+				foreach($config['js'] as $value){
+					$view->addJS($value);
+				}
 			}
 
 			// set content
@@ -46,7 +51,16 @@
 			$view->render();
 		}
 
-		public function redirect($url){
+		final protected function view($view){
+			$temp = explode('/', $view);
+			if (count($temp) > 1){
+				$view = $temp[0].DS.$temp[1];
+			}
+			
+			require_once ROOT.DS.'app'.DS.'views'.DS.$view.'.php';
+		}
+
+		final public function redirect($url){
 			header("Location: ".$url);
 		}
 	}
